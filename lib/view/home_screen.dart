7b5cbed65abe866af07dd702fa7517f2015b1e5b
luonverak <112792376+luonverak@users.noreply.github.com>
2023-11-16 18:43:22 +0700

    code commit

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:badges/badges.dart' as badges;
+import '../widget/color_item.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: MediaQuery.sizeOf(context).height / 2.3,
+            decoration: const BoxDecoration(
+              color: color,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(250),
+              ),
+            ),
+            child: ListView(
+              children: const [
+                Padding(
+                  padding: EdgeInsets.only(left: 15, right: 15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Discover',
+                        style: TextStyle(
+                          fontSize: 35,
+                          color: textColor,
+                          fontFamily: 'Lato',
+                        ),
+                      ),
+                      badges.Badge(
+                        badgeContent: Text('3'),
+                        child: Icon(
+                          Icons.shopping_cart,
+                          color: Colors.white,
+                          size: 30,
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
---
+++ lib/widget/color_item.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const Color color = Color.fromARGB(255, 255, 174, 0);
+const Color textColor = Colors.white;
