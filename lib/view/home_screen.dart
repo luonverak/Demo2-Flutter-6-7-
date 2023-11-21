57bc63fc3ebb7a0abc9d95076c748f1f1d0d1257
luonverak <112792376+luonverak@users.noreply.github.com>
2023-11-21 18:40:26 +0700

    code commit

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:demo3/model/category_model.dart';
 import 'package:flutter/material.dart';
 import 'package:badges/badges.dart' as badges;
+import 'package:simple_shadow/simple_shadow.dart';
 import '../widget/category_widget.dart';
 import '../widget/color_item.dart';
 
@@ -14,6 +15,7 @@ class HomeScreen extends StatelessWidget {
       body: Column(
         children: [
           Stack(
+            clipBehavior: Clip.none,
             children: [
               Container(
                 width: double.infinity,
@@ -69,17 +71,102 @@ class HomeScreen extends StatelessWidget {
               ),
               Positioned(
                 child: Padding(
-                  padding: const EdgeInsets.only(top: 180),
+                  padding: const EdgeInsets.only(top: 170),
                   child: CarouselSlider(
                     items: [
                       Container(
-                        width: 240,
-                        color: Colors.blue,
+                        width: 230,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          border: Border.all(color: Colors.black, width: 1),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: SingleChildScrollView(
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 10, right: 10),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                  width: double.infinity,
+                                  height: 170,
+                                  child: Center(
+                                    child: SimpleShadow(
+                                      opacity: 0.8,
+                                      color: Colors.blue,
+                                      offset: const Offset(5, 5),
+                                      sigma: 9,
+                                      child: Image.asset(
+                                        'asset/image/shoe.webp',
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                const Text(
+                                  'Cloudmonster',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: 'Lato2',
+                                  ),
+                                ),
+                                const Text(
+                                  '\$ 20.0',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: 'Lato',
+                                  ),
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    IconButton(
+                                      onPressed: () {},
+                                      icon: Container(
+                                        decoration: BoxDecoration(
+                                          color: Colors.black,
+                                          borderRadius:
+                                              BorderRadius.circular(5),
+                                        ),
+                                        child: const Padding(
+                                          padding: EdgeInsets.all(4.0),
+                                          child: Icon(
+                                            Icons.favorite,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    IconButton(
+                                      onPressed: () {},
+                                      icon: Container(
+                                        decoration: BoxDecoration(
+                                          color: Colors.black,
+                                          borderRadius:
+                                              BorderRadius.circular(5),
+                                        ),
+                                        child: const Padding(
+                                          padding: EdgeInsets.all(4.0),
+                                          child: Icon(
+                                            Icons.shopping_cart,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
                       )
                     ],
                     options: CarouselOptions(
-                      height: 280,
-                      viewportFraction: 0.7,
+                      height: 270,
+                      viewportFraction: 0.6,
                       enableInfiniteScroll: true,
                       autoPlay: true,
                       autoPlayInterval: const Duration(seconds: 5),
